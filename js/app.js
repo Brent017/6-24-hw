a6fc51a3b709cb8df536154b3c43dfc731c2df58
Brent Ottke <marieottke@Maries-MacBook-Air.local>
2019-06-24 17:01:13 -0600

    data types answered

---
+++ js/app.js
@@ -0,0 +1,52 @@
+// console.log('working')
+// A light switch that can be either on or off.
+// Array with Boolean:
+const lightSwitch = [0, 1]
+if (lightSwitch === true) {
+	return "On";
+} else {
+	return "Off";
+};
+// A user's email address.
+// Object
+const user {
+	email: "user@gmail.com"
+};
+// A spaceship with a hull, laser blasters, tractor beam, and warp drive.
+// Array
+const spaceship = ["hull", "laser blasters", "tractor beam", "warp drive"];
+// A list of student names from our class.
+// Array:
+const studentNames = ["Brent", "Josh", "John"];
+// A list of student names from our class, each with a location.
+// Object:
+const studentInfo {
+	Brent {
+		location: Westminster
+	},
+	Josh {
+		location: Denver
+	},
+	John {
+		location: Denver
+	}
+};
+// A list of student names from our class, each with a location and each with a list of favorite tv shows.
+// Object:
+const studentData {
+	Brent {
+		location: Westminster,
+		favoriteShows: ["TV show", "TV show"]
+	},
+	Josh {
+		location: Denver,
+		favoriteShows: ["TV show", "TV show"]
+	},
+	John {
+		location: Denver,
+		favoriteShows: ["TV show", "TV show"]
+	}
+};
+
+// bond question loop over the string and remove anything that is not a number, numbers go into an array, else goes into a junk array
+
