1cc4e576ed1c55b60b100e7fb136fad7f6f579e5
Brent Ottke <marieottke@Maries-MacBook-Air.local>
2019-06-24 18:23:40 -0600

    object-ception answered

--- js/app.js
+++ js/app.js
@@ -60,43 +60,65 @@
 // console.log(brent.hobby);
 
 // 3. Crazy object!:
-const crazyObject = {
-  taco: [
-    {
-      meat: 'steak',
-      cheese: ['panela', 'queso', 'chihuahua']
-    },
-    {
-      meat: 'chicken',
-      salsa: ["pico", "hot", "hotter", "really hot", "really really hot", "omg my mouth is burning"]
-    },
-  ],
-  larry: {
-    nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
-    quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", "women love a self confident bald man", "I'm a walking talking enigma"],
-    characters: [
-      {
-        name: "Jeff",
-        occupation: "manager"
-      },
-      {
-        name: "funkhauser",
-        occupation: "tv dude"
-      },
-      {
-        name: "susie",
-        occupation: "jeffs wife",
-        favourtieHobby: "Swearing at Larry and Jeff"
-      },
-    ]
-  }
+// const crazyObject = {
+//   taco: [
+//     {
+//       meat: 'steak',
+//       cheese: ['panela', 'queso', 'chihuahua']
+//     },
+//     {
+//       meat: 'chicken',
+//       salsa: ["pico", "hot", "hotter", "really hot", "really really hot", "omg my mouth is burning"]
+//     },
+//   ],
+//   larry: {
+//     nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
+//     quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", "women love a self confident bald man", "I'm a walking talking enigma"],
+//     characters: [
+//       {
+//         name: "Jeff",
+//         occupation: "manager"
+//       },
+//       {
+//         name: "funkhauser",
+//         occupation: "tv dude"
+//       },
+//       {
+//         name: "susie",
+//         occupation: "jeffs wife",
+//         favourtieHobby: "Swearing at Larry and Jeff"
+//       },
+//     ]
+//   }
+// };
+
+// console.log(crazyObject.taco[1].salsa[5]);
+// console.log(crazyObject.larry.quotes[0]);
+// console.log(crazyObject.larry.characters[2].favourtieHobby);
+// console.log(crazyObject.larry.nicknames[1]);
+// console.log(crazyObject.larry.characters[1]);
+
+// 4. Object-ception:
+const inception = {
+   reality: {
+       dreamLayer1: {
+           dreamLayer2: {
+               dreamLayer3: {
+                   dreamLayer4: {
+                       dreamLayer5: {
+                           dreamLayer6: {
+                               limbo: "Joseph Gordon Levitt"
+                          }
+                       }
+                   }
+               }
+           }
+       }
+   }
 };
 
-console.log(crazyObject.taco[1].salsa[5]);
-console.log(crazyObject.larry.quotes[0]);
-console.log(crazyObject.larry.characters[2].favourtieHobby);
-console.log(crazyObject.larry.nicknames[1]);
-console.log(crazyObject.larry.characters[1]);
+inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo = "null";
+console.log(inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo);
 
 // bond question loop over the string and remove anything that is not a number, numbers go into an array, else goes into a junk array
 
