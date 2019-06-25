36aa9bef41ed047a895a7681ea6f066ba2d56b3a
Brent Ottke <marieottke@Maries-MacBook-Air.local>
2019-06-24 18:05:30 -0600

    Commit 2 - data structures answered

--- js/app.js
+++ js/app.js
@@ -1,52 +1,67 @@
 // console.log('working')
 // A light switch that can be either on or off.
 // Array with Boolean:
-const lightSwitch = [0, 1]
-if (lightSwitch === true) {
-	return "On";
-} else {
-	return "Off";
+// const lightSwitch = [0, 1]
+// if (lightSwitch === true) {
+// 	return "On";
+// } else {
+// 	return "Off";
+// };
+// // A user's email address.
+// // Object
+// const user {
+// 	email: "user@gmail.com"
+// };
+// // A spaceship with a hull, laser blasters, tractor beam, and warp drive.
+// // Array
+// const spaceship = ["hull", "laser blasters", "tractor beam", "warp drive"];
+// // A list of student names from our class.
+// // Array:
+// const studentNames = ["Brent", "Josh", "John"];
+// // A list of student names from our class, each with a location.
+// // Object:
+// const studentInfo {
+// 	Brent {
+// 		location: Westminster
+// 	},
+// 	Josh {
+// 		location: Denver
+// 	},
+// 	John {
+// 		location: Denver
+// 	}
+// };
+// // A list of student names from our class, each with a location and each with a list of favorite tv shows.
+// // Object:
+// const studentData {
+// 	Brent {
+// 		location: Westminster,
+// 		favoriteShows: ["TV show", "TV show"]
+// 	},
+// 	Josh {
+// 		location: Denver,
+// 		favoriteShows: ["TV show", "TV show"]
+// 	},
+// 	John {
+// 		location: Denver,
+// 		favoriteShows: ["TV show", "TV show"]
+// 	}
+// };
+
+// 2. Take it Easy:
+const rainbow = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+console.log(rainbow[4]);
+const brent = {
+	favoriteFood: "burger",
+	hobby: "coin collecting",
+	location: "Westminster",
+	dataType: "Array"
 };
-// A user's email address.
-// Object
-const user {
-	email: "user@gmail.com"
-};
-// A spaceship with a hull, laser blasters, tractor beam, and warp drive.
-// Array
-const spaceship = ["hull", "laser blasters", "tractor beam", "warp drive"];
-// A list of student names from our class.
-// Array:
-const studentNames = ["Brent", "Josh", "John"];
-// A list of student names from our class, each with a location.
-// Object:
-const studentInfo {
-	Brent {
-		location: Westminster
-	},
-	Josh {
-		location: Denver
-	},
-	John {
-		location: Denver
-	}
-};
-// A list of student names from our class, each with a location and each with a list of favorite tv shows.
-// Object:
-const studentData {
-	Brent {
-		location: Westminster,
-		favoriteShows: ["TV show", "TV show"]
-	},
-	Josh {
-		location: Denver,
-		favoriteShows: ["TV show", "TV show"]
-	},
-	John {
-		location: Denver,
-		favoriteShows: ["TV show", "TV show"]
-	}
-};
+console.log(brent.hobby);
+
+
+
+
 
 // bond question loop over the string and remove anything that is not a number, numbers go into an array, else goes into a junk array
 
